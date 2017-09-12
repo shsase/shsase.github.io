1e008bf96690ddc2247f9e426200eada844fc3b7
sasebot <sasebot@gmail.com>
2017-09-12 16:04:50 +0530

    yo0

--- scripts/user.js
+++ scripts/user.js
@@ -1,52 +1,66 @@
 
-
-function headerAnim() {
-  var elem = document.getElementById("animate");
+function siteTitleAnim(doo){
+  var elem = document.getElementById("siteTitle");
   var pos = 0;
-  var id = setInterval(frame, 5);
-  function frame() {
-    if (pos == 350) {
+  var posLim = 50;
+  var id = setInterval(frame , 1);
+  var fontB = 42;
+  var fontS = 30;
+  function frame(){
+    if (pos == posLim ) {
+      clearInterval(id)
+    } else {
+      pos ++;
+      if (doo == 0){elem.style.fontSize = fontB - pos/posLim *(fontB - fontS)  + 'px';}
+      else{elem.style.fontSize =  fontS - pos/posLim *(fontS - fontB) + 'px';}
+    }
+  }
+}
+// sizeAnim(0,42,30,50,document.getElementById("siteTitle").style.fontSize)
+function sizeAnim(doo,l,s,ms,elem){
+  //var elem = document.getElementById("siteTitle");
+  var pos = 0;
+  var id = setInterval(frame , 1);
+  function frame(){
+    if (pos == ms ) {
       clearInterval(id);
     } else {
-      pos++;
-      elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
+      pos ++;
+      console.log(elem);
+      if (doo == 0){elem.style.fontSize = l - pos/ms *(l - s)  + 'px';}
+      else{elem.style.fontSize =  s - pos/ms *(s - l) + 'px';}
     }
   }
 }
-// make the meta header disappear
-function metaHeaderDisappear(){
-  var elem = document.getElementById("siteTitle");
+function sizeAnim2(doo,l,s,ms,elem){
+  //var elem = document.getElementById("siteTitle");
   var pos = 0;
   var id = setInterval(frame , 1);
+  function frame(){
+    if (pos == ms ) {
+      clearInterval(id);
+    } else {
+      pos ++;
+      console.log(elem);
+      if (doo == 0){elem.style.height = l - pos/ms *(l - s)  + 'px';}
+      else{elem.style.height =  s - pos/ms *(s - l) + 'px';}
+    }
+  }
+}
+function metaHeaderAnim(doo){
+  var elem = document.getElementById("meta-header");
+  var pos = 0;
+  var id = setInterval(frame , 0.7);
   function frame(){
     if (pos == 100 ) {
       clearInterval(id)
     } else {
       pos ++;
-      elem.style.opacity = 1 - pos/ 100;
-
+      if (doo == 0){elem.style.opacity = 1 - pos/100;}
+      else{elem.style.opacity =  pos/100;}
     }
-
   }
 }
-
-function metaHeaderAppear(){
-  var elem = document.getElementById("siteTitle");
-  var pos = 0;
-  var id = setInterval(frame , 1);
-  function frame(){
-    if (pos == 100 ) {
-      clearInterval(id)
-    } else {
-      pos ++;
-      elem.style.opacity =  pos/ 100;
-
-    }
-
-  }
-}
-
 // make the font size small
 
 var flag = 0;
@@ -58,13 +72,21 @@ function myFunction() {
         console.log('works');
 
         if (flag == 0){
-          metaHeaderDisappear();
+          // siteTitleAnim(0);
+          metaHeaderAnim(0);
+          sizeAnim(0,38,26,50,document.getElementById("siteTitle"));
+          document.getElementById("siteTitle").text='GREY MATTER'
+          sizeAnim2(0,26,7,50,document.getElementById("meta-header"));
           flag = 1;
         }
     } else {
         //document.getElementById("myP").className = "";
         if (flag == 1){
-          metaHeaderAppear();
+          // siteTitleAnim(1);
+          metaHeaderAnim(1);
+          sizeAnim(1,38,26,50,document.getElementById("siteTitle"));
+          document.getElementById("siteTitle").text='GREY MATTER MAGAZINE'
+          sizeAnim2(1,26,7,50,document.getElementById("meta-header"));
           flag = 0;
         }
     }
